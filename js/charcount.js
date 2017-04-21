04838635039ffab2ba34055702ac46639a781d78
fallenPineapple <fallenPineapple87@gmail.com>
2017-04-21 18:33:38 -0400

    Added Character Counter
    Shows the user how many characters are left before hitting the max per
    post. Currently does not work in Quick Reply.

---
+++ js/charcount.js
@@ -0,0 +1,29 @@
+/*
+ * charcount.js
+ *
+ * Usage:
+ *   $config['additional_javascript'][] = 'js/jquery.min.js';
+ *   $config['additional_javascript'][] = 'js/charcount.js';
+ *
+ */
+
+$(document).ready(function(){
+
+	// Storing this jQuery object outside of the event callback 
+	// prevents jQuery from having to search the DOM for it again
+	// every time an event is fired.
+	var $inputArea = $('#body');
+	var $coundownField = $('#countchar');
+	var $maxChars = 3600;
+        
+	// Preset countdown field to max initial content length
+	$coundownField.text($maxChars - $inputArea.val().length);
+
+	// input           :: for all modern browsers [1]
+	// selectionchange :: for IE9 [2]
+	// propertychange  :: for <IE9 [3]
+	$inputArea.on('input selectionchange propertychange', function() {
+		$charCount = $maxChars - $inputArea.val().length;
+		$coundownField.text($charCount);
+	});
+});
