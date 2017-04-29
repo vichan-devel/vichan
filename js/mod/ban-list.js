10e3cd26e022a5f6831e5bb94b90e4f9bdd22931
fallenPineapple <fallenPineapple87@gmail.com>
2017-04-29 19:25:27 -0400

    Refixed ban-list

--- js/mod/ban-list.js
+++ js/mod/ban-list.js
@@ -40,14 +40,14 @@ var banlist_init = function(token, my_boards, inMod) {
 
      // Cookie
       cookie: {
-        name: (inMod)?"Cookie":"", width: (inMod)?"180px":"0px", fmt: function(f) {
+        name: (inMod)?"Cookie":"", width: (inMod)?"180px":"5px", fmt: function(f) {
     if (!inMod) return "";
     if (!f.cookie) return "???";
         return f.cookie;
       } }, 
 
 
-      reason: {name: _("Reason"), width: (inMod)?"calc(100% - 895px - 7 * 4px)":"calc(100% - 715px - 7 * 4px)", fmt: function(f) {
+      reason: {name: _("Reason"), width: (inMod)?"calc(100% - 895px - 7 * 4px)":"calc(100% - 710px - 7 * 4px)", fmt: function(f) {
 	var add = "", suf = '';
         if (f.seen == 1) add += "<i class='fa fa-check' title='"+_("Seen")+"'></i>";
 	if (f.message) {
