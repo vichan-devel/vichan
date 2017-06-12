e6b423ba8aa166c3a087328a244c1762e045c9b4
fallenPineapple <fallenPineapple87@gmail.com>
2017-06-12 15:50:46 -0400

    Integrate shadow into install files.

--- install.php
+++ install.php
@@ -1,7 +1,7 @@
 <?php
 
 // Installation/upgrade file	
-define('VERSION', '6.0.3');
+define('VERSION', '6.0.4');
 
 require 'inc/functions.php';
 
@@ -665,7 +665,52 @@ if (file_exists($config['has_installed'])) {
 				PRIMARY KEY (`id`)
 				) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 			') or error(db_error());
-
+		case '6.0.3':
+			query('CREATE TABLE IF NOT EXISTS ``shadow_antispam`` (
+				`board` varchar(58) NOT NULL,
+				`thread` int(11) DEFAULT NULL,
+				`hash` char(40) CHARACTER SET ascii COLLATE ascii_bin NOT NULL,
+				`created` int(11) NOT NULL,
+				`expires` int(11) DEFAULT NULL,
+				`passed` smallint(6) NOT NULL,
+				PRIMARY KEY (`hash`),
+				KEY `board` (`board`,`thread`),
+				KEY `expires` (`expires`)
+				) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+			') or error(db_error());
+			query('CREATE TABLE IF NOT EXISTS ``shadow_cites`` (
+				`board` varchar(58) NOT NULL,
+				`post` int(11) NOT NULL,
+				`target_board` varchar(58) NOT NULL,
+				`target` int(11) NOT NULL,
+				KEY `target` (`target_board`,`target`),
+				KEY `post` (`board`,`post`)
+				) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+			') or error(db_error());
+			query('CREATE TABLE IF NOT EXISTS ``shadow_deleted`` (
+				`id` int(10) NOT NULL AUTO_INCREMENT,
+				`board` varchar(58) NOT NULL,
+				`post_id` int(10) NOT NULL,
+				`del_time` int(11) NOT NULL,
+				`files` text CHARACTER SET ascii NOT NULL,
+				`cite_ids` text CHARACTER SET armscii8 NOT NULL,
+				PRIMARY KEY (`id`),
+				KEY `board` (`board`),
+				KEY `post_id` (`post_id`)
+				) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+			') or error(db_error());
+			query('CREATE TABLE IF NOT EXISTS ``shadow_filehashes`` (
+				`id` int(11) NOT NULL AUTO_INCREMENT,
+				`board` varchar(58) NOT NULL,
+				`thread` int(11) NOT NULL,
+				`post` int(11) NOT NULL,
+				`filehash` text CHARACTER SET ascii NOT NULL,
+				PRIMARY KEY (`id`),
+				KEY `thread_id` (`thread`),
+				KEY `post_id` (`post`)
+				) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+			') or error(db_error());
+			
 		case false:
 			// TODO: enhance Tinyboard -> vichan upgrade path.
 			query("CREATE TABLE IF NOT EXISTS ``search_queries`` (  `ip` varchar(39) NOT NULL,  `time` int(11) NOT NULL,  `query` text NOT NULL) ENGINE=MyISAM DEFAULT CHARSET=utf8;") or error(db_error());
--- install.sql
+++ install.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: May 26, 2017 at 07:40 PM
+-- Generation Time: Jun 12, 2017 at 03:36 PM
 -- Server version: 5.7.18-0ubuntu0.16.04.1
 -- PHP Version: 7.0.18-0ubuntu0.16.04.1
 
@@ -398,6 +398,74 @@ CREATE TABLE IF NOT EXISTS `search_queries` (
   `ip` varchar(61) NOT NULL,
   `time` int(11) NOT NULL,
   `query` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shadow_antispam`
+--
+
+CREATE TABLE IF NOT EXISTS `shadow_antispam` (
+  `board` varchar(58) NOT NULL,
+  `thread` int(11) DEFAULT NULL,
+  `hash` char(40) CHARACTER SET ascii COLLATE ascii_bin NOT NULL,
+  `created` int(11) NOT NULL,
+  `expires` int(11) DEFAULT NULL,
+  `passed` smallint(6) NOT NULL,
+  PRIMARY KEY (`hash`),
+  KEY `board` (`board`,`thread`),
+  KEY `expires` (`expires`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shadow_cites`
+--
+
+CREATE TABLE IF NOT EXISTS `shadow_cites` (
+  `board` varchar(58) NOT NULL,
+  `post` int(11) NOT NULL,
+  `target_board` varchar(58) NOT NULL,
+  `target` int(11) NOT NULL,
+  KEY `target` (`target_board`,`target`),
+  KEY `post` (`board`,`post`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shadow_deleted`
+--
+
+CREATE TABLE IF NOT EXISTS `shadow_deleted` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `board` varchar(58) NOT NULL,
+  `post_id` int(10) NOT NULL,
+  `del_time` int(11) NOT NULL,
+  `files` text CHARACTER SET ascii NOT NULL,
+  `cite_ids` text CHARACTER SET armscii8 NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `board` (`board`),
+  KEY `post_id` (`post_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shadow_filehashes`
+--
+
+CREATE TABLE IF NOT EXISTS `shadow_filehashes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `board` varchar(58) NOT NULL,
+  `thread` int(11) NOT NULL,
+  `post` int(11) NOT NULL,
+  `filehash` text CHARACTER SET ascii NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `thread_id` (`thread`),
+  KEY `post_id` (`post`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
