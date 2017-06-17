5e8ec406ef83a9eab0e33d720fe0a2e6ab9511cd
Equus <equus@cock.li>
2017-06-17 12:36:53 +0200

    Add max threads per hour setting into config.php

--- post.php
+++ post.php
@@ -471,15 +471,6 @@ if (isset($_POST['delete'])) {
 	else {
 		$thread = false;
 	}
-
-
-	// Check number of threads in the last hour
-	if($thread == false && $config['threads_per_hour'] != false) {
-		$per_hour_query = query(sprintf("SELECT COUNT(*) FROM ``posts_%s`` WHERE `thread` = NULL AND `time` >= UNIX_TIMESTAMP(DATE_SUB(NOW(),INTERVAL 1 HOUR))", $board['uri']));
-		$per_hour_count = $per_hour_query->fetch(PDO::FETCH_ASSOC)[0];
-		if($per_hour_count >= $config['threads_per_hour'])
-			error($config['error']['too_many_threads']);
-	}
 	
 	// Check for an embed field
 	if ($config['enable_embedding'] && isset($_POST['embed']) && !empty($_POST['embed'])) {
