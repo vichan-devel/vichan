917b72c83e02f4b3dc4179453f1a880e34c8c6d4
gemeraldenjoyer <93288045+gemeraldenjoyer@users.noreply.github.com>
2023-06-13 04:56:40 +0100

    Create chungus.php

---
+++ chungus.php
@@ -0,0 +1,17 @@
+<TITLE>chungus</TITLE>
+<H1>
+<?php
+$phrases = [
+    "He's a big chungus",
+    "BIG BUNGUS",
+    "He will always be a big chungus",
+    "I AM A MEME OF FAT BUGS BUNNY OOH NA NA"
+];
+$randomIndex = array_rand($phrases);
+$randomPhrase = $phrases[$randomIndex];
+echo $randomPhrase;
+?>
+
+</H1>
+<!--chungus-->
+<IMG SRC=//i.imgur.com/o5xXBkj.jpg>
