fc63de4ed420cebf1d5f0bfb7862321f65ed27ed
zeke <sfpj5l17y@mozmail.com>
2024-02-01 18:05:08 -0800

    Use two Dockerfiles, move Docker stuff to /docker

---
+++ docker/common-setup.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# not exactly elegant, but one container is Debian, the other is Alpine
+useradd -MU leftypol
+addgroup leftypol
+adduser -DHG leftypol leftypol
+
+set -eu
+
+install -m 775 -o leftypol -g leftypol -d /var/www-leftypol
+ln -s \
+   /code/banners/ \
+   /code/static/ \
+   /code/stylesheets/ \
+   /code/tools/ \
+   /code/walls/ \
+   /code/*.php \
+   /code/404.html \
+   /code/LICENSE.* \
+   /code/robots.txt \
+   /code/install.sql \
+   /var/www-leftypol/
+
+install -m 775 -o leftypol -g leftypol -d /var/www/js
+ln -s /code/js/* /var/www/js/
+
+install -m 775 -o leftypol -g leftypol -d /var/www-leftypol/templates
+install -m 775 -o leftypol -g leftypol -d /var/www-leftypol/templates/cache
+ln -s /code/templates/* /var/www-leftypol/templates/
+
+install -m 775 -o leftypol -g leftypol -d /var/www-leftypol/inc
+ln -s /code/inc/* /var/www-leftypol/inc/
