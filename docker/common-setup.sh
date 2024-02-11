482962844a63d47e551d68196942542787954abf
Zankaria <zankaria.auxa@skiff.com>
2024-02-11 16:04:57 +0100

    docker: fix missing tmp directory

--- docker/common-setup.sh
+++ docker/common-setup.sh
@@ -16,8 +16,12 @@ ln -s \
    /code/install.sql \
    /var/www-leftypol/
 
-install -m 775 -o leftypol -g leftypol -d /var/www/js
-ln -s /code/js/* /var/www/js/
+install -m 775 -o leftypol -g leftypol -d /var/tmp/leftypol
+install -m 775 -o leftypol -g leftypol -d /var/tmp/leftypol/cache
+ln -s /var/tmp/leftypol /var/www-leftypol/tmp
+
+install -m 775 -o leftypol -g leftypol -d /var/www-leftypol/js
+ln -s /code/js/* /var/www-leftypol/js/
 
 install -m 775 -o leftypol -g leftypol -d /var/www-leftypol/templates
 install -m 775 -o leftypol -g leftypol -d /var/www-leftypol/templates/cache
