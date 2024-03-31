7979404c1b85c0462b16fb147ab9a49b608a5790
Zankaria <zankaria.auxa@mailu.io>
2024-03-31 15:49:09 +0200

    docker: change user to www-data

--- docker/common-setup.sh
+++ docker/common-setup.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-install -m 775 -o leftypol -g leftypol -d /var/www
+install -m 775 -o www-data -g www-data -d /var/www
 ln -s \
    /code/banners/ \
    /code/static/ \
@@ -16,16 +16,16 @@ ln -s \
    /code/install.sql \
    /var/www/
 
-install -m 775 -o leftypol -g leftypol -d /var/tmp/leftypol
-install -m 775 -o leftypol -g leftypol -d /var/tmp/leftypol/cache
+install -m 775 -o www-data -g www-data -d /var/tmp/leftypol
+install -m 775 -o www-data -g www-data -d /var/tmp/leftypol/cache
 ln -s /var/tmp/leftypol /var/www/tmp
 
-install -m 775 -o leftypol -g leftypol -d /var/www/js
+install -m 775 -o www-data -g www-data -d /var/www/js
 ln -s /code/js/* /var/www/js/
 
-install -m 775 -o leftypol -g leftypol -d /var/www/templates
-install -m 775 -o leftypol -g leftypol -d /var/www/templates/cache
+install -m 775 -o www-data -g www-data -d /var/www/templates
+install -m 775 -o www-data -g www-data -d /var/www/templates/cache
 ln -s /code/templates/* /var/www/templates/
 
-install -m 775 -o leftypol -g leftypol -d /var/www/inc
+install -m 775 -o www-data -g www-data -d /var/www/inc
 ln -s /code/inc/* /var/www/inc/
