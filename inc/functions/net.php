091c0442e8d5d0e0641b9be51e00578b9d3220ff
Lorenzo Yario <angeleno@screamer.wiki>
2024-05-11 04:42:45 -0700

    allow cloudflare and other proxies to still count as https

--- inc/functions/net.php
+++ inc/functions/net.php
@@ -6,5 +6,13 @@ namespace Vichan\Functions\Net;
  * @return bool Returns if the client-server connection is an encrypted one (HTTPS).
  */
 function is_connection_secure(): bool {
-	return !empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off';
+    if (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') {
+        return true;
+    }
+    elseif (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+        return true;
+    }
+    else {
+	return false;
+    }
 }
