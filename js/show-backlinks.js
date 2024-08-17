8d56da91083f4c56b9a45a8497c703e42a2d38ad
Zankaria <zankaria.auxa@skiff.com>
2024-08-17 23:36:33 +0200

    show-backlinks.js: format

--- js/show-backlinks.js
+++ js/show-backlinks.js
@@ -10,7 +10,6 @@
  *   $config['additional_javascript'][] = 'js/jquery.min.js';
  *   // $config['additional_javascript'][] = 'js/post-hover.js'; (optional; must come first)
  *   $config['additional_javascript'][] = 'js/show-backlinks.js';
- *
  */
 
 onReady(function() {
@@ -18,34 +17,33 @@ onReady(function() {
 		let reply_id = $(this).attr('id').replace(/(^reply_)|(^op_)/, '');
 
 		$(this).find('div.body a:not([rel="nofollow"])').each(function() {
-			let id, post, $mentioned;
-
-			if (id = $(this).text().match(/^>>(\d+)$/)) {
+			let id = $(this).text().match(/^>>(\d+)$/);
+			if (id) {
 				id = id[1];
 			} else {
 				return;
 			}
 
-			$post = $('#reply_' + id);
-			if ($post.length == 0){
-				$post = $('#op_' + id);
-				if ($post.length == 0) {
+			let post = $('#reply_' + id);
+			if (post.length == 0){
+				post = $('#op_' + id);
+				if (post.length == 0) {
 					return;
 				}
 			}
 
-			$mentioned = $post.find('p.intro span.mentioned');
-			if($mentioned.length == 0) {
-				$mentioned = $('<span class="mentioned unimportant"></span>').appendTo($post.find('p.intro'));
+			let mentioned = post.find('p.intro span.mentioned');
+			if(mentioned.length == 0) {
+				mentioned = $('<span class="mentioned unimportant"></span>').appendTo(post.find('p.intro'));
 			}
 
-			if ($mentioned.find('a.mentioned-' + reply_id).length != 0) {
+			if (mentioned.find('a.mentioned-' + reply_id).length != 0) {
 				return;
 			}
 
 			let link = $('<a class="mentioned-' + reply_id + '" onclick="highlightReply(\'' + reply_id + '\');" href="#' + reply_id + '">&gt;&gt;' +
 				reply_id + '</a>');
-			link.appendTo($mentioned)
+			link.appendTo(mentioned)
 
 			if (window.init_hover) {
 				link.each(init_hover);
@@ -54,13 +52,12 @@ onReady(function() {
 	};
 
 	$('div.post.reply').each(showBackLinks);
-	$('div.post.op').each(showBackLinks);
 
 	$(document).on('new_post', function(e, post) {
-		if ($(post).hasClass("op")) {
-			$(post).find('div.post.reply').each(showBackLinks);
+		if ($(post).hasClass('reply')) {
+			showBackLinks.call(post);
 		} else {
-			$(post).parent().find('div.post.reply').each(showBackLinks);
+			$(post).find('div.post.reply').each(showBackLinks);
 		}
 	});
 });
