13b587cf62aa8caf8826bbf85c9846e287a2e4af
fowr <89118232+perdedora@users.noreply.github.com>
2024-08-20 21:27:22 -0300

    add resource_version to avoid cache.

--- inc/secrets.php
+++ inc/secrets.php
@@ -1,1 +1,7 @@
 <?php
+
+	$config['db']['type']		= 'mysql';
+	$config['db']['server']		= 'localhost';
+	$config['db']['user']		= 'dev';
+	$config['db']['password']	= 'dev';
+	$config['db']['database']	= 'vichan';
