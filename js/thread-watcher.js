1fb6ddceed6808b17e950f2dda2cc1f403dd73da
Zankaria <zankaria.auxa@mailu.io>
2024-10-01 01:14:31 +0200

    thread-watcher.js: add support for new ui

--- js/thread-watcher.js
+++ js/thread-watcher.js
@@ -153,8 +153,8 @@ $(document).ready(function() {
 
 	// Append the watchlist toggle button.
 	$('.boardlist').append('<div class="watchlist-container" style="float:right;"><a class="watchlist-toggle" href="#">[' + _('Watchlist') + ']</a></div>');
-	// Append a watch thread button after every OP post number.
-	$('.op>.intro>.post_no:odd').after('<a class="watchThread" href="#">[' + _('Watch Thread') + ']</a>');
+	// Append a watch thread button in the user controls container.
+	$('div.post.op>.intro>.user-controls').append('<a class="watchThread" href="#">[' + _('Watch Thread') + ']</a>');
 
 	// Draw the watchlist, hidden.
 	watchlist.render();
