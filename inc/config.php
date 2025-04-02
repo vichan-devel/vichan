16a6112b5b694e033415fad814e5d15e0d9b26bb
Lorenzo Yario <angeleno@screamer.wiki>
2025-04-02 02:45:04 -0500

    new embed stuff

--- inc/config.php
+++ inc/config.php
@@ -1164,8 +1164,45 @@
 			'<iframe style="float: left; margin: 10px 20px;" width="%%tb_width%%" height="%%tb_height%%" frameborder="0" src="https://www.dailymotion.com/embed/video/$2" allowfullscreen></iframe>'
 		),
 		array(
-			'/^https?:\/\/(\w+\.)?metacafe\.com\/watch\/(\d+)\/([a-zA-Z0-9_\-.]+)\/(\?[^\'"<>]+)?$/i',
-			'<iframe style="float: left; margin: 10px 20px;" width="%%tb_width%%" height="%%tb_height%%" frameborder="0"  src="https://www.metacafe.com/embed/$2/$3/" allowfullscreen></iframe>'
+			'/^https?:\/\/(www\.)?rumble\.com\/(?:embed\/)?([a-zA-Z0-9]+)(?:\/[a-zA-Z0-9\-]+\.html)?(?:\?[^\'"<>]*)?$/i',
+			'<iframe class="rumble" width="%%tb_width%%" height="%%tb_height%%" src="https://rumble.com/embed/$2/" frameborder="0" allowfullscreen></iframe>'
+		),
+		array(
+			'/^https?:\/\/(www\.)?bitchute\.com\/(?:video|embed)\/([a-zA-Z0-9]+)(\/)?(\?[^\'"<>]*)?$/i',
+			'<iframe allowfullscreen="true" width="%%tb_width%%" height="%%tb_height%%" scrolling="no" frameborder="0" style="border: none;" src="https://www.bitchute.com/embed/$2"></iframe>'
+		),
+		array(
+			'/^https?:\/\/(www\.)?kick\.com\/([a-zA-Z0-9_]+)(\?[^\'"<>]*)?$/i',
+			'<iframe src="https://kick.com/embed/$2" frameborder="0" allowfullscreen="true" scrolling="no" height="%%tb_height%%" width="%%tb_width%%"></iframe>'
+		),
+		array(
+			'/^https?:\/\/(www\.)?odysee\.com\/(?:\$\/embed\/)?(?:@[^:\/]+:[a-zA-Z0-9]+\/)?[a-zA-Z0-9\-]+:([a-zA-Z0-9]+)(\/)?(\?[^\'"<>]*)?$/i',
+			'<iframe width="%%tb_width%%" height="%%tb_height%%" src="https://odysee.com/$/embed/$2" allowfullscreen></iframe>'
+		),
+		/*
+		array(
+			'/^https?:\/\/(www\.)?tiktok\.com\/@([a-zA-Z0-9_.]+)\/video\/([0-9]+)(\?[^\'"<>]*)?$/i',
+			'<blockquote class="tiktok-embed" cite="https://www.tiktok.com/@$2/video/$3" data-video-id="$3" style="max-width: %%tb_width%%px;min-width: 325px;"><section></section></blockquote><script async src="https://www.tiktok.com/embed.js"></script>'
+		),
+		array(
+			'/^https?:\/\/(www\.)?instagram\.com\/(p|reel|tv)\/([a-zA-Z0-9_-]+)(\/)?(\?[^\'"<>]*)?$/i',
+			'<blockquote class="instagram-media" data-instgrm-permalink="https://www.instagram.com/$2/$3/" data-instgrm-version="14" style="max-width: %%tb_width%%px; min-width: 326px; width: 100%;"></blockquote><script async src="//www.instagram.com/embed.js"></script>'
+		),
+		array(
+			'/^https?:\/\/(www\.)?(twitter|x)\.com\/([a-zA-Z0-9_]+)\/status\/([0-9]+)(\?[^\'"<>]*)?$/i',
+			'<blockquote class="twitter-tweet"><a href="https://x.com/$3/status/$4"></a></blockquote><script async src="https://platform.x.com/widgets.js"></script>'
+		),
+		array(
+			'/^https?:\/\/(www\.)?bsky\.app\/profile\/([a-zA-Z0-9_.-]+)\/post\/([a-zA-Z0-9]+)(\?[^\'"<>]*)?$/i',
+			'<iframe src="https://embed.bsky.app/profile/$2/post/$3" width="%%tb_width%%" height="300" style="border:none; overflow:hidden;" scrolling="no"></iframe>'
+		),*/
+		array(
+			'/^https?:\/\/(www\.)?bilibili\.com\/video\/(BV[a-zA-Z0-9]+|av[0-9]+)(\/)?(\?[^\'"<>]*)?$/i',
+			'<iframe width="%%tb_width%%" height="%%tb_height%%" src="https://player.bilibili.com/player.html?bvid=$2" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"></iframe>'
+		),
+		array(
+			'/^https?:\/\/(www\.)?(nicovideo\.jp\/watch|nico\.ms)\/(sm[0-9]+)(\?[^\'"<>]*)?$/i',
+			'<iframe width="%%tb_width%%" height="%%tb_height%%" src="https://embed.nicovideo.jp/watch/$3" frameborder="0" allowfullscreen></iframe>'
 		),
 		array(
 			'/^https?:\/\/(\w+\.)?(vocaroo\.com\/|voca\.ro\/)([a-zA-Z0-9]{2,12})$/i',
