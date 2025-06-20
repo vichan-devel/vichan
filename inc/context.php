e7d48e5990bfc15c23c9c5d7e30f3997f665455d
fowr <89118232+perdedora@users.noreply.github.com>
2025-06-20 10:15:50 -0300

    context.php: update dependencies of service

--- inc/context.php
+++ inc/context.php
@@ -130,7 +130,9 @@ function build_context(array $config): Context {
 			);
 		},
 		BannersService::class => fn(Context $c): BannersService => new BannersService(
-			$c->get(CacheDriver::class)
+			$c->get('config')['banner_ext'],
+			$c->get(CacheDriver::class),
+			$c->get(LogDriver::class)
 		),
 	]);
 }
