a87dbbd8c5dbe1a93015b413baea610bdb98acc3
fowr <89118232+perdedora@users.noreply.github.com>
2025-06-20 10:13:50 -0300

    b.php: remove try/catch

--- b.php
+++ b.php
@@ -5,12 +5,7 @@ use Vichan\Service\BannersService;
 require_once 'inc/bootstrap.php';
 use function Vichan\build_context;
 
-try {
-	$board = htmlspecialchars($_GET['board'] ?? $config['banner_ukko'], ENT_QUOTES, 'UTF-8');
-	$ctx = build_context($config);
-	$banners = $ctx->get(BannersService::class);
-	$banners->serve($board);
-} catch (InvalidArgumentException) {
-	http_response_code(400);
-	exit;
-}
+$board = htmlspecialchars($_GET['board'] ?? $config['banner_ukko'], ENT_QUOTES, 'UTF-8');
+$ctx = build_context($config);
+$banners = $ctx->get(BannersService::class);
+$banners->serve($board);
