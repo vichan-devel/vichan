276fc69e23b608b8a242991fdcda9a1f38988601
Zankaria <zankaria.auxa@mailu.io>
2025-07-23 21:19:35 +0200

    pages.php: gzip buffering of json ban stream was causing excessive latency

--- inc/mod/pages.php
+++ inc/mod/pages.php
@@ -1247,9 +1247,6 @@ function mod_bans_json(Context $ctx) {
 	if (!hasPermission($config['mod']['ban']))
 		error($config['error']['noaccess']);
 
-	// Compress the json for faster loads
-	if (substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip')) ob_start("ob_gzhandler");
-
 	Bans::stream_json(false, !hasPermission($config['mod']['view_banstaff']), $mod['boards']);
 }
 
