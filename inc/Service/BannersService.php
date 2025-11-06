b1e0d9997b2cca4be447e041f28f9fb0b8152d9d
Zankaria <zankaria.auxa@skiff.com>
2025-11-06 15:00:53 +0100

    BannersService.php: add argument to control priority banners likelihood

--- inc/Service/BannersService.php
+++ inc/Service/BannersService.php
@@ -10,6 +10,7 @@ class BannersService {
 	private const BANNERS_DIR = 'static/banners/';
 	private const PRIORITY_DIR = 'static/banners_priority/';
 	private LogDriver $logger;
+	private int $priority_denominator;
 
 	private static function isImage(string $fileName): bool {
 		// For speed reasons, we trust the extension.
@@ -27,8 +28,14 @@ class BannersService {
 		exit;
 	}
 
-	public function __construct(LogDriver $logger) {
+	/**
+	 * @param LogDriver $logger Driver to write logs
+	 * @param int $priority_denominator The denominator over the likelihood of a priory banner being chosen.
+	 *                                  Must be >= 0. Use 0 to disable priority banners (except as a fallback).
+	 */
+	public function __construct(LogDriver $logger, int $priority_denominator) {
 		$this->logger = $logger;
+		$this->priority_denominator = $priority_denominator;
 	}
 
 	/**
@@ -65,7 +72,7 @@ class BannersService {
 	}
 
 	public function serve(string $subdir): void {
-		$usePriority = empty($subdir) || \mt_rand(0, 3) === 0;
+		$usePriority = empty($subdir) || ($this->priority_denominator > 0 && \mt_rand(0, $this->priority_denominator) === 0);
 
 		if (!$usePriority) {
 			$bannerDir = self::BANNERS_DIR . $subdir . '/';
